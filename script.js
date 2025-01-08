46face7a51ac72bbc2564dbf2f38d36e9ea21744
Daojim <jimmyddao@gmail.com>
2025-01-07 20:54:03 -0500

    Create getComputerChoice() and logic for it

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  let computerChoice;
+  let rng = Math.round(Math.random() * 100);
+  console.log(rng);
+  if (rng >= 0 && rng <= 33) {
+    computerChoice = "rock";
+  }
+  if (rng >= 34 && rng <= 66) {
+    computerChoice = "paper";
+  }
+  if (rng >= 67 && rng <= 100) {
+    computerChoice = "scissors";
+  }
+  console.log(computerChoice);
+}
+
+getComputerChoice();
