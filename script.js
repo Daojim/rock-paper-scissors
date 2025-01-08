851cf9160a2d31e18a7233441b9771961132a12b
Daojim <jimmyddao@gmail.com>
2025-01-07 21:14:21 -0500

    Add function for user's choice for RPS

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+//Function for computer picking
 function getComputerChoice() {
-  let computerChoice;
-  let rng = Math.round(Math.random() * 100);
-  console.log(rng);
+  let computerChoice; //Create variable for computer's choice
+  let rng = Math.round(Math.random() * 100); //An RNG variable, multiply by 100 because Math.random is 0 to <1
+  console.log(rng); //See what number is generated and give computerChoice a string value depending on number
   if (rng >= 0 && rng <= 33) {
     computerChoice = "rock";
   }
@@ -15,3 +16,24 @@ function getComputerChoice() {
 }
 
 getComputerChoice();
+
+//Function for user picking
+function getHumanChoice() {
+  let valid = 0;
+  let humanChoice;
+  while (valid < 1) {
+    humanChoice = prompt("rock, paper, or scissors?");
+    if (
+      humanChoice.toLowerCase() === "rock" ||
+      humanChoice.toLowerCase() === "paper" ||
+      humanChoice.toLowerCase() === "scissors"
+    ) {
+      valid = 1;
+    } else {
+      console.log("Invalid choice, try again");
+    }
+  }
+  console.log(humanChoice);
+}
+
+getHumanChoice();
